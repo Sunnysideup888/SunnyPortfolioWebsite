bb730866b25230844dd75624cb273ca958a9e896
Sunny Chen <z5587681@ad.unsw.edu.au>
2025-08-27 21:33:22 +1000

    Added database methods with Amazon RDS

--- backend/src/main/java/com/sunnyportfoliowebsite/backend/controller/Controller.java
+++ backend/src/main/java/com/sunnyportfoliowebsite/backend/controller/Controller.java
@@ -1,12 +1,40 @@
 package com.sunnyportfoliowebsite.backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sunnyportfoliowebsite.backend.model.User;
+import com.sunnyportfoliowebsite.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class Controller {
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/api/test")
     public String test() {
         return "Backend works just fine and this is awesome and so cool help me";
+    }
+
+    @GetMapping("/api/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/api/users")
+    public User addUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/api/users/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        return userRepository
+                .findById(id)
+                .map(user -> {
+                    userRepository.delete(user);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
---
+++ backend/src/main/java/com/sunnyportfoliowebsite/backend/model/User.java
@@ -0,0 +1,18 @@
+package com.sunnyportfoliowebsite.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private LocalDate dob;
+    private String interests;
+}
