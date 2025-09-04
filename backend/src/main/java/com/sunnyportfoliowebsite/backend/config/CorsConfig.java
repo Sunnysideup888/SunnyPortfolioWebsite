5c87b9bd497ebd24fe7e430f78e49485dccf2054
Sunny Chen <z5587681@ad.unsw.edu.au>
2025-09-04 23:18:23 +1000

    Modify CORS Spring Boot to only run on dev profile, not on EC2 instance

--- backend/src/main/java/com/sunnyportfoliowebsite/backend/config/CorsConfig.java
+++ backend/src/main/java/com/sunnyportfoliowebsite/backend/config/CorsConfig.java
@@ -1,22 +1,24 @@
-//package com.sunnyportfoliowebsite.backend.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//    @Value("${ALLOWED_ORIGINS}")
-//    private String allowedOrigins;
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/api/**")
-//                .allowedOrigins(allowedOrigins)
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(true)
-//                .maxAge(3600);
-//    }
-//}
+package com.sunnyportfoliowebsite.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("dev")
+public class CorsConfig implements WebMvcConfigurer {
+    @Value("${ALLOWED_ORIGINS}")
+    private String allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
